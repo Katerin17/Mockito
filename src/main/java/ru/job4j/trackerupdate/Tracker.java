94f2052e98116ebb8c2f1bfe8a2d21262c7b5fab
Ekaterina Mironenko <Krivets86@mail.ru>
2021-03-21 09:01:13 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/trackerupdate/Tracker.java
@@ -0,0 +1,82 @@
+package ru.job4j.trackerupdate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tracker implements Store {
+
+    private static Tracker instance = null;
+
+    public static Tracker getInstance() {
+        if (instance == null) {
+            instance = new Tracker();
+        }
+        return instance;
+    }
+
+    private Tracker() {
+    }
+
+    private final List<Item> items = new ArrayList<>();
+
+    private int ids = 1;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items.add(item);
+        return item;
+    }
+    @Override
+    public List<Item> findAll() {
+        return items;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items.get(index) : null;
+    }
+    @Override
+    public List<Item> findByName(String key) {
+        List<Item> result = new ArrayList<>();
+        for (Item item : items) {
+            if (item.getName().equals(key)) {
+                result.add(item);
+            }
+        }
+        return result;
+    }
+    @Override
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            item.setId(id);
+            items.set(index, item);
+        }
+        return result;
+    }
+    @Override
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            items.remove(index);
+        }
+        return result;
+    }
+
+    private int indexOf(int id) {
+        int result = -1;
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId() == id) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public void clear() {
+        items.clear();
+    }
+}
